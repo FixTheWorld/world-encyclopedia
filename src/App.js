cd2d0c91cd1c3437312ab57b68a810ecc5f577fe
forceking3 <2561037140@qq.com>
2017-07-01 10:59:09 +0800

    delete

--- src/App.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by changjin.zhang on 6/29/2017.
- */
-import React from 'react';
-export default ({children})=>{
-    return (
-        <div style={{height:"100%"}}>{children}</div>
-    )
-};
